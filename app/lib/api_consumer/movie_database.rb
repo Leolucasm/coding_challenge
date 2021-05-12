ca86bde597757db2a3db11f456a808f23659060b
Leonardo Melo <leolucasm@hotmail.com>
2021-05-12 09:23:42 -0300

    Ajustando Estrutura Inicial Projeto

---
+++ app/lib/api_consumer/movie_database.rb
@@ -0,0 +1,3 @@
+class ApiConsumer::MovieDatabase # https://developers.themoviedb.org/3
+
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "coding_challeng_production"
+  # config.active_job.queue_name_prefix = "coding_challenge_production"
 
   config.action_mailer.perform_caching = false
 
